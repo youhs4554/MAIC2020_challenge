ec854e18f212c36d034cb7ee4e98ebf2d95180cd
hossay <youhs4554@gmail.com>
2020-09-11 01:07:44 +0000

    shufflenet v2

---
+++ models/shufflenet.py
@@ -0,0 +1,157 @@
+import torch
+import torch.nn as nn
+
+class ShuffleV2Block(nn.Module):
+    def __init__(self, in_planes, out_planes, mid_channels, *, kernel_size, stride):
+        super(ShuffleV2Block, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+
+        self.mid_channels = mid_channels
+        self.kernel_size = kernel_size
+        pad = kernel_size // 2
+        self.pad = pad
+        self.in_planes = in_planes
+
+        outputs = out_planes - in_planes
+
+        branch_main = [
+            nn.Conv2d(in_planes, mid_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(mid_channels, mid_channels, kernel_size=kernel_size, stride=stride, padding=pad, groups=mid_channels, bias=False),
+            nn.BatchNorm2d(mid_channels),
+
+            nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(outputs),
+            nn.ReLU(inplace=True)
+        ]
+        self.branch_main = nn.Sequential(*branch_main)
+
+        if stride == 2:
+            branch_proj = [
+                nn.Conv2d(in_planes, in_planes, kernel_size, stride, pad, groups=in_planes, bias=False),
+                nn.BatchNorm2d(in_planes),
+                nn.Conv2d(in_planes, in_planes, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(in_planes),
+                nn.ReLU(inplace=True)
+            ]
+            self.branch_proj = nn.Sequential(*branch_proj)
+        else:
+            self.branch_proj = None
+
+    def forward(self, old_x):
+        if self.stride == 1:
+            x_proj, x = self.channel_shuffle(old_x)
+            return torch.cat((x_proj, self.branch_main(x)), 1)
+        elif self.stride == 2:
+            x_proj = old_x
+            x = old_x
+            #import ipdb; ipdb.set_trace()
+            return torch.cat((self.branch_proj(x_proj), self.branch_main(x)), 1)
+
+    def channel_shuffle(self, x):
+        batch_size, num_channels, height, width = x.data.size()
+
+        assert (num_channels % 4 == 0)
+        #
+        x = x.reshape(batch_size * num_channels // 2, 2, height * width)
+        x = x.permute(1, 0, 2)
+        x = x.reshape(2, -1, num_channels // 2, height, width)
+        return x[0], x[1]
+
+
+class ShuffleNetV2(nn.Module):
+    def __init__(self, n_class=1000, model_size='0.5x'):
+        super(ShuffleNetV2, self).__init__()
+        print ('model size is ', model_size)
+
+        self.stage_repeats = [4 ,8, 4]
+        self.model_size = model_size
+
+        if model_size == '0.5x':
+            self.stage_out_channels = [-1, 24, 48, 96, 192, 1024]
+        elif model_size == '1.0x':
+            self.stage_out_channels = [-1, 24, 116, 232, 464, 1024]
+        elif model_size == '1.5x':
+            self.stage_out_channels = [-1, 24, 176, 352, 704, 1024]
+        elif model_size == '2.0x':
+            self.stage_out_channels = [-1, 24, 244, 488, 976, 2048]
+        else:
+            raise NotImplementedError
+
+
+        input_channel = self.stage_out_channels[1]
+        self.first_conv = nn.Sequential(
+            nn.Conv2d(1, input_channel, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(input_channel),
+            nn.ReLU(inplace=True)
+        )
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.features = []
+        for idxstage in range(len(self.stage_repeats)):
+            numrepeat = self.stage_repeats[idxstage]
+            output_channel = self.stage_out_channels[idxstage+2]
+
+            for i in range(numrepeat):
+                if i == 0:
+                    self.features.append(ShuffleV2Block(input_channel, output_channel, mid_channels=output_channel//2, kernel_size=3, stride=2))
+                else:
+                    self.features.append(ShuffleV2Block(input_channel // 2, output_channel, mid_channels=output_channel//2, kernel_size=3, stride=1))
+                input_channel = output_channel
+
+        self.features = nn.Sequential(*self.features)
+
+        self.conv_last = nn.Sequential(
+            nn.Conv2d(input_channel, self.stage_out_channels[-1], 1, 1, 0, bias=False),
+            nn.BatchNorm2d(self.stage_out_channels[-1]),
+            nn.ReLU(inplace=True)
+        )
+
+        self.globalpool = nn.AdaptiveAvgPool2d((1,1))
+        self.dropout = nn.Dropout(0.2)
+        self.fc = nn.Linear(self.stage_out_channels[-1], 1, bias=False)
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.first_conv(x)
+        x = self.maxpool(x)
+        x = self.features(x)
+        x = self.conv_last(x)
+
+        x = self.globalpool(x)
+        x = self.dropout(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        x = torch.sigmoid(x)
+
+        return x
+
+    def _initialize_weights(self):
+        for name, m in self.named_modules():
+            if isinstance(m, nn.Conv2d):
+                if 'first' in name:
+                    nn.init.normal_(m.weight, 0, 0.01)
+                else:
+                    nn.init.normal_(m.weight, 0, 1.0 / m.weight.shape[1])
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            if isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.0001)
+                nn.init.constant_(m.running_mean, 0)
+            if isinstance(m , nn.BatchNorm1d):
+                nn.init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.0001)
+                nn.init.constant_(m.running_mean, 0)
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+def shufflenet_v2():
+    return ShuffleNetV2(n_class=1)
